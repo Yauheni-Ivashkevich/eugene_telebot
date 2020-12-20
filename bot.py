384c032f31935ca3145d759a2e7b010f4501ba03
Yauheni-Ivashkevich <zheklex@yandex.ru>
2020-12-20 17:20:52 +0300

    Update files

--- bot.py
+++ bot.py
@@ -69,7 +69,7 @@ def callback_worker(call):
         bot.send_message(call.message.chat.id, "Приятно познакомиться! ")  # код сохранения данных, или их обработки
         bot.send_message(call.message.chat.id, "Безусловно, жизнь есть жизнь, и в ней всему есть место!")
         bot.send_message(call.message.chat.id, "Но, не забывайте про нас! ")
-        bot.send_message(call.message.chat.id, "Всего хорошего! : )")
+        bot.send_message(call.message.chat.id, "Всего хорошего, дорогой друг! : )")
         bot.send_sticker(call.message.chat.id, "CAACAgIAAxkBAANiX8rBH9xFQJxek_aiof8d76-7GHkAAtQIAAIItxkCBaNkOqPpyIoeBA",)
     elif call.data == "no":
         bot.send_message(call.message.chat.id, "Попробуем еще раз!")
