1047e509d8d2a7a679b700a7b00ab1967acdb614
Yauheni-Ivashkevich <zheklex@ya.ru>
2020-12-22 22:52:49 +0300

    Update files

--- bot.py
+++ bot.py
@@ -65,7 +65,7 @@ def reg_age(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-    if (call.data == "yes"):  # call.data это callback_data, которую мы указали при объявлении кнопки
+    if call.data == "yes":  # call.data это callback_data, которую мы указали при объявлении кнопки
         bot.send_message(call.message.chat.id, "Приятно познакомиться! ")  # код сохранения данных, или их обработки
         bot.send_message(call.message.chat.id, "Безусловно, жизнь есть жизнь, и в ней всему есть место!")
         bot.send_message(call.message.chat.id, "Но, не забывайте про нас! Вместе мы достигнем многого! ")
