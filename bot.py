6606e51339eaa1244ac0f26c97568ff50780533f
Yauheni Ivashkevich <zheklex@ya.ru>
2021-12-02 23:57:00 +0300

    Updated Procfile

--- bot.py
+++ bot.py
@@ -2,6 +2,9 @@ import config
 import telebot
 from pyowm import OWM
 
+# bot token from @BotFather
+TOKEN = "2146819907:AAGcW3jIDaTSYjl0DcVMOi81GpBradaTDVY"
+API_KEY = "a42a5fb479fd085ee548319b6976642a"
 
 bot = telebot.TeleBot(config.TOKEN)
 
