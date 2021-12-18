484f462bfabb22d72a7cb74832fdbb4a7b60e71f
Yauheni Ivashkevich <zheklex@ya.ru>
2021-12-18 22:36:58 +0300

    Comments def gen_markup in bot.py

--- bot.py
+++ bot.py
@@ -29,24 +29,24 @@ def url(message):
     bot.send_message(message.chat.id, "Всегда актуальный прогноз для Минска", reply_markup=markup)
 
 
-def gen_markup():
-    markup = InlineKeyboardMarkup()
-    markup.row_width = 2
-    markup.add(InlineKeyboardButton("Yes", callback_data="cb_yes"),
-               InlineKeyboardButton("No", callback_data="cb_no"))
-
-
-@bot.callback_query_handler(func=lambda call: True)
-def callback_query(call):
-    if call.data == "cb_yes":
-        bot.answer_callback_query(call.id, "Answer is Yes")
-    elif call.data == "cb_no":
-        bot.answer_callback_query(call.id, "Answer is No")
-
-
-@bot.message_handler(func=lambda message: True)
-def message_handler(message):
-    bot.send_message(message.chat.id, "Yes/no?", reply_markup=gen_markup())
+# def gen_markup():
+#     markup = InlineKeyboardMarkup()
+#     markup.row_width = 2
+#     markup.add(InlineKeyboardButton("Yes", callback_data="cb_yes"),
+#                InlineKeyboardButton("No", callback_data="cb_no"))
+#
+#
+# @bot.callback_query_handler(func=lambda call: True)
+# def callback_query(call):
+#     if call.data == "cb_yes":
+#         bot.answer_callback_query(call.id, "Answer is Yes")
+#     elif call.data == "cb_no":
+#         bot.answer_callback_query(call.id, "Answer is No")
+#
+#
+# @bot.message_handler(func=lambda message: True)
+# def message_handler(message):
+#     bot.send_message(message.chat.id, "Yes/no?", reply_markup=gen_markup())
 
 
 # Обработчик команд '/start' и '/help'.
